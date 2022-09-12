42881a75a0809c7b28bfb6db29a9f671e24aaae9
Nipun Bahri <67367858+BahriNipun@users.noreply.github.com>
2022-09-12 16:27:23 +0200

    Update test-iac.py

--- iac/tests/python/test-iac.py
+++ iac/tests/python/test-iac.py
@@ -1,6 +1,7 @@
-def add(x, y):
-    return x + y
-
-
-def test_add():
-    assert add(1, 2) == 3
+    def setUp(self):
+        self.seq = list(range(10))
+    
+    def test_readme_existence(self):   
+        # test if there is a README.md file
+        readme_path = './{}/README.md'.format(os.environ['BUILD_REPOSITORY_NAME'])
+        self.assertTrue(os.path.exists(readme_path), 'Readme file({}) should exists.'.format(readme_path)) 
