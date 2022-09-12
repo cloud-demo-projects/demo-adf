1b94ef0ac0ca086c20d3fc9cd01363e691991696
Nipun Bahri <bahri.nipun@gmail.com>
2022-09-12 16:52:05 +0200

    sa

--- iac/tests/python/test-iac.py
+++ iac/tests/python/test-iac.py
@@ -1,7 +1,8 @@
-    def setUp(self):
-        self.seq = list(range(10))
-    
-    def test_readme_existence(self):   
-        # test if there is a README.md file
-        readme_path = './{}/README.md'.format(os.environ['BUILD_REPOSITORY_NAME'])
-        self.assertTrue(os.path.exists(readme_path), 'Readme file({}) should exists.'.format(readme_path)) 
+def setUp(self):
+    self.seq = list(range(10))
+
+
+def test_readme_existence(self):
+    # test if there is a README.md file
+    readme_path = './{}/README.md'.format(os.environ['BUILD_REPOSITORY_NAME'])
+    self.assertTrue(os.path.exists(readme_path), 'Readme file({}) should exists.'.format(readme_path))
