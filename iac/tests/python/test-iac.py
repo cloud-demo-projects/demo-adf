915c46d719f147313a1c6392df8eb7bcc929d7e9
Nipun Bahri <bahri.nipun@gmail.com>
2022-09-12 17:16:11 +0200

    sa

--- iac/tests/python/test-iac.py
+++ iac/tests/python/test-iac.py
@@ -1,16 +1,22 @@
-def setUp(self):
-    self.seq = list(range(10))
+import random
+import glob
+import unittest
+import os
 
 
-def test_readme_existence(self):
-    # test if there is a README.md file
-    readme_path = './{}/README.md'.format(os.environ['BUILD_REPOSITORY_NAME'])
-    self.assertTrue(os.path.exists(readme_path), 'Readme file({}) should exists.'.format(readme_path))
+class TestModule(unittest.TestCase):
 
+    def setUp(self):
+        self.seq = list(range(10))
 
-def test_template_files(self):
-    bicep_files = glob.glob('./{}/template/*.bicep'.format(os.environ['BUILD_REPOSITORY_NAME']))
-    self.assertGreater(len(bicep_files), 0, 'There should be at least one bicep file in the template folder.')
+    def test_readme_existence(self):
+        # test if there is a README.md file
+        readme_path = './{}/README.md'.format(os.environ['BUILD_REPOSITORY_NAME'])
+        self.assertTrue(os.path.exists(readme_path), 'Readme file({}) should exists.'.format(readme_path))
 
-    bicep_config_path = './{}/template/bicepconfig.json'.format(os.environ['BUILD_REPOSITORY_NAME'])
-    self.assertTrue(os.path.exists(bicep_config_path), 'The bicepconfig.json file({}) should exists.'.format(bicep_config_path)) 
+    def test_template_files(self):
+        bicep_files = glob.glob('./{}/template/*.bicep'.format(os.environ['BUILD_REPOSITORY_NAME']))
+        self.assertGreater(len(bicep_files), 0, 'There should be at least one bicep file in the template folder.')
+
+        bicep_config_path = './{}/template/bicepconfig.json'.format(os.environ['BUILD_REPOSITORY_NAME'])
+        self.assertTrue(os.path.exists(bicep_config_path), 'The bicepconfig.json file({}) should exists.'.format(bicep_config_path))
