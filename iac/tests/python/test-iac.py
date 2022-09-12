b7b3ea8df72474acd1aca57a29fbfb00663ab2b2
Nipun Bahri <67367858+BahriNipun@users.noreply.github.com>
2022-09-12 17:10:18 +0200

    Update test-iac.py

--- iac/tests/python/test-iac.py
+++ iac/tests/python/test-iac.py
@@ -6,3 +6,11 @@ def test_readme_existence(self):
     # test if there is a README.md file
     readme_path = './{}/README.md'.format(os.environ['BUILD_REPOSITORY_NAME'])
     self.assertTrue(os.path.exists(readme_path), 'Readme file({}) should exists.'.format(readme_path))
+    
+    
+def test_template_files(self):
+    bicep_files = glob.glob('./{}/template/*.bicep'.format(os.environ['BUILD_REPOSITORY_NAME']))
+    self.assertGreater(len(bicep_files), 0, 'There should be at least one bicep file in the template folder.')
+
+    bicep_config_path = './{}/template/bicepconfig.json'.format(os.environ['BUILD_REPOSITORY_NAME'])
+    self.assertTrue(os.path.exists(bicep_config_path), 'The bicepconfig.json file({}) should exists.'.format(bicep_config_path)) 
